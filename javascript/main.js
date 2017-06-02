80a170d80e69d4139d6e79d2f90c1cb71def93a8
Michael Yang <themichaelyang@gmail.com>
2017-06-01 20:57:53 -0400

    added parallax scroll

--- javascript/main.js
+++ javascript/main.js
@@ -1,27 +1,35 @@
 window.onload = () => {
-
   let starElement = document.getElementById('stars')
+  let canvases = []
+  let speeds = [1, 0.5, 0.25, 0.1, 0]
 
-  for (let i = 0; i < 4; i++) {
-    starElement.appendChild(createStarCanvas())
+  for (let i = 1; i <= 5; i++) {
+    let canvas = createStarCanvas(speeds[i], (5 - i) * 100, starElement.offsetWidth, starElement.offsetHeight)
+
+    canvases.push(canvas)
+    starElement.appendChild(canvas)
   }
+
+  window.addEventListener('scroll', function(event) {
+    canvases.map((canvas) => canvas.updateScroll(window.scrollY))
+  })
 
 }
 
-function createStarCanvas(scrollSpeed) {
+function createStarCanvas(scrollSpeed, numStars, width, height) {
   let canvas = document.createElement('canvas')
-  canvas.width = document.documentElement.clientWidth
-  canvas.height = document.documentElement.clientHeight
-  canvas.className = 'stars'
+  canvas.width = width
+  canvas.height = height
+  canvas.className = 'star-canvas'
 
   let context = canvas.getContext('2d')
 
-  context.fillStyle = 'rgba(0, 0, 0, 0.4)'
+  context.fillStyle = 'rgba(0, 0, 0, 0.25)'
   context.fillRect(0, 0, canvas.width, canvas.height)
 
   context.fillStyle = 'white'
 
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < numStars; i++) {
     let x = Math.random() * canvas.width
     let y = Math.random() * canvas.height
 
@@ -29,6 +37,10 @@ function createStarCanvas(scrollSpeed) {
     drawCircle(x, y, 1, context)
     context.closePath()
     context.fill()
+  }
+
+  canvas.updateScroll = (scrollDistance) => {
+    canvas.style.top = scrollDistance * scrollSpeed + 'px'
   }
 
   return canvas
